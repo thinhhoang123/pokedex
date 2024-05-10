5b3eafa604088a64f50102ee9fe1f46e82b83977
ThinhHoang <atulala4657@gmail.com>
2024-05-10 21:02:36 +0700

    feat: Update load more and scroll to top

--- src/app/search/_components/PokemonListItem.tsx
+++ src/app/search/_components/PokemonListItem.tsx
@@ -6,17 +6,25 @@ import Image from 'next/image';
 import uniqBy from 'lodash/uniqBy';
 import generateBgColor from '@/utils/generateBgColor';
 import { Badge } from '@/components/ui/badge';
+import PokemonCard from './PokemonCard';
+import { Button } from '@/components/ui/button';
 
 export default function PokemonListItem({
   pokemonList,
 }: {
   pokemonList: any[];
 }) {
+  const numberOfPokemonEachLoad = 30;
   const [pokemons, setPokemons] = useState(() => {
     return uniqBy(pokemonList, 'id');
   });
-  const [pokemonData, setPokemonData] = useState(pokemons.slice(0, 10));
+  const [loadPokemon, setLoadPokemon] = useState(numberOfPokemonEachLoad);
+
+  const [pokemonData, setPokemonData] = useState(
+    pokemons.slice(0, loadPokemon)
+  );
   const query = useStoreSearchPokemon((state) => state.query);
+  const maxPokemon = pokemons.length - 1;
 
   useEffect(() => {
     if (query) {
@@ -25,62 +33,29 @@ export default function PokemonListItem({
       });
       setPokemonData(filteredPokemon);
     } else {
-      setPokemonData(pokemons.slice(0, 10));
+      setPokemonData(pokemons.slice(0, loadPokemon));
     }
-  }, [query]);
+  }, [query, loadPokemon]);
 
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 w-full">
-      {pokemonData.map((pokemon, index) => {
-        return (
-          <div
-            key={index}
-            className={`relative rounded-md aspect-square flex flex-col items-center justify-center cursor-pointer ${generateBgColor(
-              pokemon.type[0]
-            )}`}
-          >
-            <Image
-              src={`/typeIcon/${pokemon.type[0]}.svg`}
-              width={250}
-              height={250}
-              alt={pokemon.ThumbnailAltText}
-              loader={({ src, width, quality }) => {
-                return `${src}?w=${width}&q=${quality || 75}`;
-              }}
-              className="absolute -z-5 opacity-10"
-            />
-            <div className="flex">
-              {pokemon.type.map((type: string) => {
-                return (
-                  <Image
-                    src={`/type/${type}.svg`}
-                    width={50}
-                    height={50}
-                    alt={pokemon.ThumbnailAltText}
-                    loader={({ src, width, quality }) => {
-                      return `${src}?w=${width}&q=${quality || 75}`;
-                    }}
-                    className="drop-shadow-xl"
-                    key={type}
-                  />
-                );
-              })}
-            </div>
-
-            <Image
-              src={pokemon.ThumbnailImage}
-              width={150}
-              height={150}
-              alt={pokemon.ThumbnailAltText}
-              loader={({ src, width, quality }) => {
-                return `${src}?w=${width}&q=${quality || 75}`;
-              }}
-              className="drop-shadow-lg"
-            />
-            <Badge className="z-10">{pokemon.ThumbnailAltText}</Badge>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 w-full">
+        {pokemonData.map((pokemon, index) => {
+          return <PokemonCard pokemon={pokemon} key={pokemon.id} />;
+        })}
+      </div>
+      {loadPokemon < maxPokemon ? (
+        <Button
+          className="mb-2 rounded-full "
+          onClick={() =>
+            setLoadPokemon((prev) => {
+              return prev + numberOfPokemonEachLoad;
+            })
+          }
+        >
+          Load more
+        </Button>
+      ) : null}
+    </>
   );
 }
