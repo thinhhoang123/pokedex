7c38cf663b2de2a63309f28367212ee0573e68c9
Dong Nguyen <dong.nguyen@nbcuni.com>
2024-11-04 10:50:19 +0700

    Remove cache

--- src/service/getPokemon.ts
+++ src/service/getPokemon.ts
@@ -1,5 +1,7 @@
 const getAllPokemon = async () => {
-  const response = await fetch('https://www.pokemon.com/us/api/pokedex/kalos');
+  const response = await fetch('https://www.pokemon.com/us/api/pokedex/kalos', {
+    cache: 'no-store',
+  });
   if (!response.ok) return '';
   const data = await response.json();
   return data;
